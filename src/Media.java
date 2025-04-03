f30d055f03d609804ee8159b004904fe836a772c
Linus-llm <linusmolgaard@gmail.com>
2025-04-03 11:30:53 +0200

    Added abstract media + search class

---
+++ src/Media.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public abstract class Media {
+    protected String name;
+    protected int releaseYear;
+    protected ArrayList<String> genre;
+    protected double imdbRating;
+
+    public void playMedia(){
+
+    }
+    private void exitOptions(){
+
+    }
+}
---
+++ src/Search.java
@@ -0,0 +1,18 @@
+public class Search {
+
+    public void searchOptions(){
+
+    }
+    private void searchByText(){
+
+    }
+    private void searchMovie(){
+
+    }
+    private void searchSeries(){
+
+    }
+    private void searchGenre(){
+
+    }
+}
