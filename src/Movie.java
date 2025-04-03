209176562edda25a1d9d6f35d1b6a9958f78d92a
NikooNM <nikoonamnabavi@gmail.com>
2025-04-03 11:37:56 +0200

    Har oprettet en Movie og Series klasse, tilføjet attributter og getters

---
+++ src/Movie.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Movie {
+    private ArrayList<String> moviesList;
+
+    public ArrayList<String> getMovies(){
+        return this.moviesList;
+    }
+}
---
+++ src/Series.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Series {
+    int[][] seriesSeasons;
+    ArrayList<String> seriesList;
+
+    public ArrayList<String> getSeries(){
+        return this.seriesList;
+    }
+}
