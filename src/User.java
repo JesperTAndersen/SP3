2cc786a93a5a4e89020b3d352bdd19fe723e721d
JesperTAndersen <axicon89@gmail.com>
2025-04-03 11:33:08 +0200

    User class

---
+++ src/User.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+public class User {
+    public String name;
+    private boolean isKid = false;
+    private ArrayList<String> haveWatched;
+    private ArrayList<String> myList;
+
+    public User(String name){
+        this.name = name;
+        haveWatched = new ArrayList<>();
+        myList = new ArrayList<>();
+    }
+
+    public User(String name, boolean isKid){
+        this.name = name;
+        this.isKid = isKid;
+        haveWatched = new ArrayList<>();
+        myList = new ArrayList<>();
+    }
+
+    public boolean getIsKid(){
+        return this.isKid;
+    }
+
+    public ArrayList<String> getHaveWatched() {
+        return this.haveWatched;
+    }
+
+    public ArrayList<String> getMyList() {
+        return this.myList;
+    }
+
+
+}
