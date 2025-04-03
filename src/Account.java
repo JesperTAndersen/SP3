6b30881cf7be5b2a605f928556c1486977816fb1
JesperTAndersen <axicon89@gmail.com>
2025-04-03 12:46:58 +0200

    Startmenu stuff

--- src/Account.java
+++ src/Account.java
@@ -1,16 +1,22 @@
 import java.util.ArrayList;
 
 public class Account {
+    FileIO io = new FileIO();
+    TextUI ui = new TextUI();
+
     private String username;
     private String password;
     private ArrayList<User> users;
+    private ArrayList<Account> accounts = new ArrayList<>();
 
     Account(String username, String password){
         this.username = username;
         this.password = password;
     }
 
-    public void createAccount(){}
+    public void addAccount(Account a){
+        accounts.add(a);
+    }
 
     public User getUsers(){
         for (User u : users){
--- src/StartMenu.java
+++ src/StartMenu.java
@@ -1,9 +1,33 @@
+import java.io.File;
 import java.util.ArrayList;
 
 public class StartMenu {
-    private ArrayList<Account> accounts;
+    TextUI ui = new TextUI();
+    FileIO io = new FileIO();
+
+    private ArrayList<String> accData;
+
 
     void login(){
+        ui.displayMessage("Velkommen til Chill");
+        int userChoice = ui.promptNumeric("1. Login \n2. Opret Konto \n");
 
+
+        switch (userChoice){
+            case 1:
+
+                    String username = ui.promptText("Indtast Brugernavn: ");
+                    String password = ui.promptText("Indtast Password: ");
+
+
+
+            case 2:
+                username = ui.promptText("Indtast Brugernavn: ");
+                password = ui.promptText("Indtast Password: ");
+                createAccount(username, password);
+
+                break;
+
+        }
     }
 }
