34a70a93b591f9b293d31c9805ec1fdaabb1881a
JesperTA <axicon89@gmail.com>
2025-04-09 10:24:02 +0200

    Minor edit

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -49,13 +49,10 @@ public class MainMenu {
             }
             int userChoiceNum = ui.promptNumeric("Vælg hvilken film/serie du vil se fra din liste: ");
 
-            /*if(userChoiceNum > user.getMyList().size()||userChoiceNum<0){
-                userChoiceNum = ui.promptNumeric("Ugyldigt valg prøv igen: ");
-            }*/
-
             //linjen tjekker om userChoiceNum er større end listen's størrelse eller om userChoiceNum er mindre end 0
             userChoiceNum = ui.promptIfNumCheck(userChoiceNum,user.getMyList().size());
             m.playMedia(user, user.getMyList().get(userChoiceNum - 1));
+            user.getMyList().remove(userChoiceNum-1);
         }
     }
 
